45054a43b3aaec4196f084ac98318e32c20db020
Steve Harris <steve.harris@aistemos.com>
2025-07-07 18:29:48 +0100

    Added lots of features

--- desktop/monitor.py
+++ desktop/monitor.py
@@ -48,11 +48,29 @@ def get_track_times():
     return track_pos, track_len
 
 
+def abbreviate(s: str, max_len=24):
+    if s is None:
+        return ''
+    if len(s) > max_len:
+        if ' - ' in s:
+            parts = s.split(' - ')
+            return parts[0]
+        if '(' in s:
+            parts = s.split('(')
+            return parts[0]
+        if ' / ' in s:
+            parts = s.split(' / ')
+            return parts[0]
+        if len(s) > max_len * 2 + 3:
+            return s[:max_len*2] + '…'
+    return s
+
+
 def send_all(sio):
     track_pos, track_len = get_track_times()
     track_artist = run_as('get artist of current track')
-    track_album = run_as('get album of current track')
-    track_name = run_as('get name of current track')
+    track_album = abbreviate(run_as('get album of current track'))
+    track_name = abbreviate(run_as('get name of current track'))
     print()
     print(track_artist)
     print(track_album)
@@ -100,6 +118,7 @@ def main(argv):
                 break
         if ser_device:
             ser = serial.Serial(ser_device, baudrate=115200, timeout=1)
+            print(f'Connected to {ser}')
             monitor(ser)
         else:
             print('Unable to find active USB serial interface', file=sys.stderr)
@@ -129,34 +148,61 @@ def monitor(ser):
                         playing = send_state(sio, is_playing(sio))
                         last_track_name = send_all(sio)
                     case 'LL':
-                        print('List... [not implemented]')
+                        playlists = run_as_list('get name of every playlist')
+                        sio.write('OP ' + '|'.join(playlists) + '\n')
+                        sio.flush();
                     case 'FA':
+                        print('Favourite')
                         run_as('''get current track
-    set favorited of current track to true''')
+set favorited of current track to true''')
                     case 'SK':
                         run_as('play next track')
                         last_track_name = send_all(sio)
                         playing = send_state(sio, True)
+                    case 'PL':
+                        print('Playing playlist', arg)
+                        run_as(f'''set shuffle enabled to false
+set thePlaylist to playlist "{arg}"
+play thePlaylist''')
+                        playing = send_state(sio, True)
+                        time.sleep(1)
+                        last_track_name = send_all(sio)
+                    case 'SH':
+                        print('Shuffle playlist', arg)
+                        run_as(f'''set shuffle enabled to true
+set shuffle mode to albums
+set thePlaylist to playlist "{arg}"
+play thePlaylist''')
+                    case 'JU':
+                        print('Jump', arg)
+                        run_as(f'set player position to (player position {arg})')
+                        send_track_pos(sio)
+                    case 'SY':
+                        print('Syncing')
+                        last_track_name = send_all(sio)
+                        playing = send_state(sio, is_playing(sio))
+                        last_sync = time.time()
                     case '!!':
                         print('Arduino:', arg)
                     case _:
                         print(f'Unknown command: {cmd}')
 
-            track_name = run_as('get name of current track')
-            if track_name != last_track_name:
-                send_all(sio)
-                playing = send_state(sio, is_playing(sio))
-                last_track_name = track_name
-            else:
-                playing_now = is_playing(sio)
-                if playing_now != playing:
-                    playing = send_state(sio, playing_now)
             now = time.time()
-            if playing and now > last_sync + 10:
-                send_track_pos(sio)
+            if now > last_sync + 10:
+                track_name = run_as('get name of current track')
+                if track_name != last_track_name:
+                    send_all(sio)
+                    playing = send_state(sio, is_playing(sio))
+                    last_track_name = track_name
+                else:
+                    playing_now = is_playing(sio)
+                    if playing_now != playing:
+                        playing = send_state(sio, playing_now)
+                if playing:
+                    send_track_pos(sio)
                 last_sync = now
-        except serial.serialutil.SerialException:
-            print('Lost connection to serial device')
+        except serial.serialutil.SerialException as e:
+            print(f'Lost connection to serial device: {e}', file=sys.stderr)
             return
 
 if __name__ == '__main__':
